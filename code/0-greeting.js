44dca59e824eac33bfc751617b58f8348db3124d
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-05-22 22:49:12 +0000

    joke and modification done

--- code/0-greeting.js
+++ code/0-greeting.js
@@ -3,11 +3,15 @@
 // ####################################
 
 function greeting() {
-  let username = prompt("What's your name?")
+  let username = prompt("Whats's your name?")
 
-  alert('Hello ' + username)
+  alert('Kind of a bad name ngl')
 }
 
 function joke() {
   // write the code for your joke here.
+
+alert(' I love telling jokes about orphans. ')
+
+alert('What are they going to do, tell their parents?')
 }
